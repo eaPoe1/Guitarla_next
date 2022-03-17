bc80c967175732e29c5628a961976e375004e0e9
Matias Beroni <matiasbertoni2@gmail.com>
2022-03-17 14:45:11 -0300

    strapi guitar_la

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '826bff9227936435255e381be20be306'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ce131883-5735-449a-a0d9-fb15f80ae8a7'
+};
